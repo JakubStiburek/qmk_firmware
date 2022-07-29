c512fe54f377aaa4fd2f61f6a8d9f45ae252760a
Jakub Stibůrek <alcolp96@gmail.com>
2022-07-29 10:44:56 +0200

    init

---
+++ keyboards/splitkb/kyria/keymaps/JakubStiburek/rules.mk
@@ -0,0 +1,6 @@
+OLED_ENABLE = yes
+OLED_DRIVER = SSD1306       # Enables the use of OLED displays
+ENCODER_ENABLE = yes        # Enables the use of one or more encoders
+# RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
+UNICODE_ENABLE = yes        # Enables the use of unicode
+TAP_DANCE_ENABLE = yes      # Enables the tap dance feature
