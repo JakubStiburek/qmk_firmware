abda002be67a1f0bbb73ca317e81e5f74ed2c5f3
Jakub Stibůrek <alcolp96@gmail.com>
2022-12-28 13:57:00 +0100

    lgui on meor layer

--- keyboards/splitkb/kyria/keymaps/JakubS/keymap.c
+++ keyboards/splitkb/kyria/keymaps/JakubS/keymap.c
@@ -77,7 +77,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT(
      KC_TAB  , KC_Q ,  KC_W   ,  KC_E  ,   KC_R  ,   KC_T  ,                                                           KC_Y  ,   KC_U ,  KC_I   ,   KC_O ,  KC_P   , KC_BSPC,
      KC_LGUI , KC_A ,  KC_S   ,  KC_D  ,   KC_F  ,   KC_G  ,                                                           KC_H  ,   KC_J ,  KC_K   ,   KC_L , KC_SCLN , KC_ENT,
-     KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V  ,   KC_B  , KC_CAPS , KC_HYPR  , KC_PGUP         , LOGOUT             , KC_N  ,   KC_M , KC_COMM , KC_DOT , KC_SLSH , KC_RSFT,
+     KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V  ,   KC_B  , KC_CAPS , KC_HYPR  , DM_PLY1       , LOGOUT             , KC_N  ,   KC_M , KC_COMM , KC_DOT , KC_SLSH , KC_RSFT,
                                 KC_ESC , KC_LALT , KC_LCTL , KC_SPC  , MO(_NUM) , MO(_FUNCTION) , LT(_MEOR,KC_EQL)   , KC_PLUS ,  KC_BSLS , SWITCH_LANG
     ),
  /*
@@ -87,8 +87,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      [_NUM] = LAYOUT(
       KC_TRNS  , KC_TILD ,  KC_P7   ,  KC_P8  ,   KC_P9 ,   KC_P0   ,                                           KC_AT  ,   KC_LPRN ,    KC_RPRN ,   KC_HASH , KC_PERC , KC_TRNS,
       KC_TRNS  , KC_PSLS ,  KC_P4   ,  KC_P5  ,   KC_P6 ,   KC_PMNS ,                                          KC_SLSH ,   KC_LBRC ,    KC_RBRC ,   KC_BSLS , KC_ASTR , KC_TRNS,
-      KC_TRNS  , KC_PAST ,  KC_P1   ,  KC_P2  ,   KC_P3 ,   KC_PPLS , KC_NO   , KC_TRNS , KC_TRNS , KC_TRNS ,  KC_DLR  ,    KC_LT  ,     KC_GT  ,   KC_AMPR , KC_MINS , KC_TRNS,
-                                      KC_TRNS , KC_TRNS ,   KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS ,  KC_EXLM ,   KC_QUOT ,     KC_GRV
+      KC_TRNS  , KC_PAST ,  KC_P1   ,  KC_P2  ,   KC_P3 ,   KC_PPLS , KC_NO   , KC_TRNS , DM_REC1 , KC_TRNS ,  KC_DLR  ,    KC_LT  ,     KC_GT  ,   KC_AMPR , KC_MINS , KC_TRNS,
+                                      KC_TRNS , KC_TRNS ,   KC_TRNS , KC_TRNS , KC_TRNS , DM_RSTP , KC_TRNS ,  KC_EXLM ,   KC_QUOT ,     KC_GRV
      ),
 /*
   * Layer: FUNCTION
--- keyboards/splitkb/kyria/keymaps/JakubS/rules.mk
+++ keyboards/splitkb/kyria/keymaps/JakubS/rules.mk
@@ -1,4 +1,5 @@
 ENCODER_ENABLE = yes       # Enables the use of one or more encoders
+DYNAMIC_MACRO_ENABLE = yes # Enables Dynamic Macros
 # OLED_ENABLE = yes
 # OLED_DRIVER = SSD1306   # Enables the use of OLED displays
 
