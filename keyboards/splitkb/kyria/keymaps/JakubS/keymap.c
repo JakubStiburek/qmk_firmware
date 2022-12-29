962bbe835906cd45c8428b2e93862d1c6575419c
Jakub Stibůrek <alcolp96@gmail.com>
2022-12-29 20:20:21 +0100

    meor 2

--- keyboards/splitkb/kyria/keymaps/JakubS/keymap.c
+++ keyboards/splitkb/kyria/keymaps/JakubS/keymap.c
@@ -77,7 +77,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT(
      KC_TAB  , KC_Q ,  KC_W   ,  KC_E  ,   KC_R  ,   KC_T  ,                                                    KC_Y  ,   KC_U ,  KC_I   ,   KC_O ,  KC_P   , KC_BSPC,
      KC_LGUI , KC_A ,  KC_S   ,  KC_D  ,   KC_F  ,   KC_G  ,                                                    KC_H  ,   KC_J ,  KC_K   ,   KC_L , KC_SCLN , KC_ENT,
-     KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V  ,   KC_B  , KC_CAPS , KC_HYPR  , KC_BSLS        , LOGOUT    ,   KC_N  ,   KC_M , KC_COMM , KC_DOT , KC_SLSH , KC_RSFT,
+     KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V  ,   KC_B  , KC_CAPS , KC_HYPR  , KC_BSLS       , LOGOUT    ,   KC_N  ,   KC_M , KC_COMM , KC_DOT , KC_SLSH , KC_RSFT,
                                 KC_ESC , KC_LALT , KC_LCTL , KC_SPC  , MO(_NUM) , MO(_FUNCTION) , MO(_MEOR) , KC_EQL  ,  KC_PLUS , SWITCH_LANG
     ),
  /*
